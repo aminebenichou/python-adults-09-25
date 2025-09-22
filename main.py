33c2772735333e4fadd2278204ce52fe2c769002
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2025-09-22 18:52:42 +0100

    python adults sept25

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+small_list_1=[1, 2, 3]
+small_list_2=[5, 7]
+small_list_3=[9, 10]
+big_list=[small_list_1, small_list_2, small_list_3]
+k=0
+while k<3:
+    v=0
+    while v<len(big_list[k]):
+        print(big_list[k][v])
+        v += 1
+    k += 1
