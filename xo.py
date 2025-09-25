75c8b3ac83805a32d6c41a6c64a2feb7f79fe476
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2025-09-25 17:17:20 +0100

    sync

--- xo.py
+++ xo.py
@@ -1,18 +1,52 @@
 cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+player = "X"
+wins= [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+running = True
+while running:
+    print(f" {cells[0]} | {cells[1]} | {cells[2]} ")
+    print("___|___|___")
+    print(f" {cells[3]} | {cells[4]} | {cells[5]} ")
+    print("___|___|___")
+    print(f" {cells[6]} | {cells[7]} | {cells[8]} ")
+    print("   |   |   ")
+    i=0
+    player_input = input("Enter a number between 1-9: ")
+    while i<len(cells):
+        if int(player_input) == cells[i]:
+            cells[i]=player
+        i += 1
+    if player=="X":
+        player="O"
+    else:
+        player="X"
 
-print(f" {cells[0]} | {cells[1]} | {cells[2]} ")
-print("___|___|___")
-print(f" {cells[3]} | {cells[4]} | {cells[5]} ")
-print("___|___|___")
-print(f" {cells[6]} | {cells[7]} | {cells[8]} ")
-print("   |   |   ")
+    # k=0
+    # while k<7:
+    #     if cells[k]==cells[k+1]==cells[k+2]:
+    #         print("You Win")
+    #     k += 3
+    # k=0
+    # while k<3:
+    #     if cells[k]==cells[k+3]==cells[k+6]:
+    #         print("You Win")
+    #     k += 1
 
-if cells[0]==1:
-    cells[0]="X"
-    
-print(f" {cells[0]} | {cells[1]} | {cells[2]} ")
-print("___|___|___")
-print(f" {cells[3]} | {cells[4]} | {cells[5]} ")
-print("___|___|___")
-print(f" {cells[6]} | {cells[7]} | {cells[8]} ")
-print("   |   |   ")
+    # if (cells[0]==cells[4]==cells[8]) or (cells[2]==cells[4]==cells[6]) :
+    #     print("You Win")
+
+    for win in wins:
+        if cells[win[0]]==cells[win[1]]==cells[win[2]]:
+            print("You win")
+            running=False
+        
+
+   
