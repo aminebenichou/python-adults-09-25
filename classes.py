4ec910fdf39caf92a60191b36664d7ba451937ac
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2025-09-28 19:57:09 +0100

    sync

--- classes.py
+++ classes.py
@@ -10,6 +10,41 @@ class BankAccount:
         self.amount = self.amount+(amount-(amount*(rate/100)))
         
     def withdraw(self, amount, rate):
+        if amount>self.amount:
+            print("Add some money before withdrawing")
+            return
         self.amount = self.amount-(amount)
         self.amount = self.amount-(amount*(rate/100))
+
         
+    def __str__(self):
+        return str(self.amount)
+    
+    class subAccount:
+        parent_id=""
+    
+my_account=BankAccount('Amine', 100)
+my_account.calculate_interest(2.5)
+print(my_account)
+my_account.deposit(1000, 0.3)
+print(my_account)
+my_account.withdraw(5000, 1)
+print(my_account)
+my_account.subAccount().parent_id
+
+
+class Product:
+    title: str
+    desc: str
+    price: float
+    quantity: int
+
+
+class Cart:
+    products: list[Product]
+
+
+    def add(self, product:Product):
+        self.products.append(product)
+        
+
